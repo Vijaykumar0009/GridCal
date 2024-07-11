377963f3ed4c04a427321c13c655570a06849092
PK. Vijay Kumar <143599757+Vijaykumar0009@users.noreply.github.com>
2024-07-12 00:31:23 +0530

    Add files via upload

---
+++ doc/auto_document_models.py
@@ -0,0 +1,321 @@
+import os
+from typing import Dict
+import pandas as pd
+from pytablewriter import RstSimpleTableWriter
+from GridCalEngine.IO.cim.cgmes.cgmes_circuit import CgmesCircuit
+from GridCalEngine.IO.raw.devices.psse_circuit import PsseCircuit
+from GridCalEngine.Devices.multi_circuit import MultiCircuit
+from GridCalEngine.IO.gridcal.pack_unpack import get_objects_dictionary
+from GridCalEngine.enumerations import CGMESVersions
+
+
+def get_cgmes_data_frames():
+    """
+
+    :return:
+    """
+    info = dict()
+    circuit = CgmesCircuit(cgmes_version=CGMESVersions.v2_4_15)
+
+    for class_name, class_type in circuit.cgmes_assets.class_dict.items():
+
+        cls = class_type("", class_name)
+
+        data = list()
+        for property_name, cim_property in cls.declared_properties.items():
+            data.append(cim_property.get_dict())
+
+        info[class_name] = pd.DataFrame(data=data)
+
+    return info
+
+
+def get_psse_data_frames():
+    """
+
+    :return:
+    """
+    info = dict()
+
+    circuit = PsseCircuit()
+
+    for prop in circuit.get_properties():
+
+        if prop.class_type not in [str, bool, int, float]:
+
+            cls = prop.class_type
+            class_name = str(cls).split('.')[-1].split(' ')[0].replace("'>", "")
+            data = list()
+            obj = cls()
+            for cls_prop in obj.get_properties():
+                data.append(cls_prop.get_dict())
+
+            info[class_name] = pd.DataFrame(data=data)
+
+    return info
+
+
+def get_gridcal_data_frames():
+    """
+
+    :return:
+    """
+    info = dict()
+
+    obj_dict = get_objects_dictionary()
+
+    circuit = MultiCircuit()
+    d = circuit.objects_with_profiles.copy()
+    categories = {elm.device_type.value: cat for cat, elms in d.items() for elm in elms}
+    for obj_type_name, obj in obj_dict.items():
+
+        class_name = obj.device_type.value
+
+        data = list()
+        for prop_name, prop in obj.registered_properties.items():
+            data.append(prop.get_dict())
+
+        info[class_name] = pd.DataFrame(data=data)
+
+    return info
+
+
+def get_gridcal_data_frames_per_category():
+    """
+
+    :return:
+    """
+    info = dict()
+
+    obj_dict = get_objects_dictionary()
+
+    circuit = MultiCircuit()
+    d = circuit.objects_with_profiles.copy()
+    categories = {elm.device_type.value: cat for cat, elms in d.items() for elm in elms}
+
+    for obj_type_name, obj in obj_dict.items():
+
+        class_name = obj.device_type.value
+        category = categories.get(class_name, None)
+
+        if category is not None:
+            data = list()
+            for prop_name, prop in obj.registered_properties.items():
+                data.append(prop.get_dict())
+
+            df = pd.DataFrame(data=data)
+
+            category_dict = info.get(category, None)
+
+            if category_dict:
+                category_dict[class_name] = df
+            else:
+                info[category] = {class_name: df}
+
+    return info
+
+
+def write_dataframes_to_excel(data_frames: Dict[str, pd.DataFrame], filename):
+    """
+
+    :param data_frames:
+    :param filename:
+    :return:
+    """
+    with pd.ExcelWriter(filename) as w:
+        for key, df in data_frames.items():
+            df.to_excel(w, sheet_name=key)
+
+
+def write_dataframes_to_excel_one_sheet(data_frames: Dict[str, pd.DataFrame], filename):
+    """
+
+    :param data_frames:
+    :param filename:
+    :return:
+    """
+    df_all = pd.DataFrame()
+    for key, df in data_frames.items():
+        # df['class'] = key
+        df.insert(0, 'class', key)
+        df_all = pd.concat([df_all, df], axis=0)
+
+    with pd.ExcelWriter(filename) as w:
+        df_all.to_excel(w, sheet_name='ALL_CLASSES')
+
+
+def write_dataframes_to_rst(data_frames: Dict[str, pd.DataFrame], filename, tilte):
+    """
+
+    :param data_frames:
+    :param filename:
+    :param tilte:
+    :return:
+    """
+    m = 10
+
+    names = list(data_frames.keys())
+    names.sort()
+
+    with open(filename, 'w') as w:
+
+        w.write("Models\n")
+        w.write("=============\n\n")
+
+        w.write(tilte + "\n")
+        w.write("-" * (len(tilte) * m) + "\n\n")
+
+        for key in names:
+            df = data_frames[key]
+
+            if df.shape[0] > 0:
+                writer = RstSimpleTableWriter(dataframe=df)
+                s = writer.dumps()
+            else:
+                s = "No table info."
+
+            w.write(key + "\n")
+            w.write("^" * (len(tilte) * m) + "\n\n")
+            w.write(s)
+            w.write("\n\n")
+
+
+def write_dataframes_to_latex(data_frames: Dict[str, Dict[str, pd.DataFrame]], folder="data_model",
+                              cols_to_delete=['max_chars', 'comment', 'mandatory']):
+    """
+
+    :param data_frames:
+    :param filename:
+    :param tilte:
+    :return:
+    """
+
+    if not os.path.exists(folder):
+        os.makedirs(folder)
+
+    def fix(val):
+        return str(val).replace("_", "\_").replace("%", "\%")
+
+    with open(os.path.join(folder, 'data_models.tex'), 'w') as wp:
+
+        for cat_name, dataframes_dict in data_frames.items():
+
+            cat_fname = "data_model_" + cat_name.replace(" & ", "_").lower()
+            fname = os.path.join(folder, f'{cat_fname}.tex')
+
+            wp.write("\\input{" + folder + "/" + cat_fname + "}\n")
+
+            with open(fname, 'w') as w:
+
+                w.write("\section{" + cat_name.replace("&", "and") + "}\n")
+
+                for key, df in dataframes_dict.items():
+
+                    w.write("\n\\textbf{" + str(key) + "}\n\n")
+
+                    if len(cols_to_delete):
+                        df = df.drop(columns=cols_to_delete)
+
+                    hdr = [fix(col) for col in df.columns.tolist()]
+                    idx = [fix(col) for col in df.index.tolist()]
+                    data = df.values.copy()
+                    for i in range(data.shape[0]):
+                        for j in range(data.shape[1]):
+                            data[i, j] = fix(data[i, j])
+
+                    frmt = "|".join(["X" for h in hdr])
+                    text = "\\begin{fullwidth}\n"
+                    text += "\\small\n"
+                    text += "\\begin{longtable}{|X|X|X|X|}\n"
+                    text += "\\caption{" + str(key) + "}\n"
+                    text += "\\toprule\n"
+                    text += " & ".join(hdr) + "\\\\ \n"
+                    text += "\\midrule\n"
+
+                    nrows = data.shape[0]
+                    for i in range(nrows):
+                        text += " & ".join(data[i, :])
+                        text += "\\\\ \n"
+                        # if i < (nrows - 1):
+                        #     text += "\\\\ \n"
+                        # else:
+                        #     text += "\n"
+                    text += "\\bottomrule\n"
+                    text += "\end{longtable}\n"
+                    text += "\end{fullwidth}\n"
+
+                    w.write(text + "\n")
+                    w.write("\n\n")
+
+
+def write_dataframes_to_rst2(w, data_frames: Dict[str, pd.DataFrame], tilte):
+    """
+
+    :param w:
+    :param data_frames:
+    :param tilte:
+    :return:
+    """
+    m = 10
+
+    names = list(data_frames.keys())
+    names.sort()
+
+    w.write(tilte + "\n")
+    w.write("-" * (len(tilte) * m) + "\n\n")
+
+    for key in names:
+        df = data_frames[key]
+
+        if df.shape[0] > 0:
+            writer = RstSimpleTableWriter(dataframe=df)
+            s = writer.dumps()
+        else:
+            s = "No table info."
+
+        w.write(key + "\n")
+        w.write("^" * (len(tilte) * m) + "\n\n")
+        w.write(s)
+        w.write("\n\n")
+
+
+def write_models_to_rst(filename):
+    """
+
+    :param filename:
+    :return:
+    """
+    with open(filename, 'w') as w:
+        w.write("Data Models\n")
+        w.write("==========================\n\n")
+
+        cgmes_info = get_cgmes_data_frames()
+        psse_info = get_psse_data_frames()
+        gridcal_info = get_gridcal_data_frames()
+
+        write_dataframes_to_rst2(w, cgmes_info, "CGMES")
+        write_dataframes_to_rst2(w, psse_info, "PSSE")
+        write_dataframes_to_rst2(w, gridcal_info, "GridCal")
+
+
+if __name__ == '__main__':
+    # cgmes_info = get_cgmes_data_frames()
+    # psse_info = get_psse_data_frames()
+    gridcal_info = get_gridcal_data_frames()
+    gridcal_info_cat = get_gridcal_data_frames_per_category()
+
+    # write_dataframes_to_excel_one_sheet(cgmes_info,
+    #                           'cgmes_classes_all_in_one_sheet.xlsx')
+    # write_dataframes_to_excel_one_sheet(psse_info,
+    #                           'psse_classes_all_in_one_sheet.xlsx')
+    write_dataframes_to_excel_one_sheet(gridcal_info,
+                              'roseta_classes_all_in_one_sheet.xlsx')
+
+    # write_dataframes_to_rst(cgmes_info, 'cgmes_clases.rst', "CGMES")
+    # write_dataframes_to_rst(psse_info, 'psse_clases.rst', "PSSE")
+    # write_dataframes_to_rst(roseta_info, 'roseta_clases.rst', "Roseta")
+    # write_dataframes_to_latex(gridcal_info_cat)
+
+    # write_models_to_rst(os.path.join('rst_source', 'other_data_models.rst'))
+
+    print("done")
---
+++ doc/index.rst
@@ -0,0 +1,89 @@
+Welcome to GridCal's documentation!
+===================================
+
+GridCal is a top tier power systems planning and simulation software. As
+such it has all the static analysis studies that you can think of, plus
+linear and non-linear optimization functions. Some of these functions
+are well known, while others you may have never heard of as they are a
+product of cutting-edge research.
+
+.. figure:: rst_source/figures/GridCal.png
+    :alt: GridCal main figure
+
+|Codacy Badge| |Documentation Status| |Build Status| |DOI| |Downloads|
+
+GridCal started in 2015 with a clear objective: create a solid
+programming library and a user-friendly interface. This straightforward
+approach sparked many innovations — some driven by the necessity for
+commercial use, and others fueled by curiosity and research.
+
+Whether you’re a pro needing free tools, a researcher wanting a
+real-world tested platform, a teacher sharing commercial-grade software
+insights, or a student diving into practical algorithms, GridCal’s got
+your back. It’s a high quality product made for all of us now and for
+the future generations.
+
+Contact
+=======
+
+-  Join the `Discord GridCal
+   channel <https://discord.com/invite/dzxctaNbvu>`__ for a friendly
+   chat, or quick question.
+-  Submit questions or comments to our
+   `form <https://forms.gle/MpjJAntAwZiLwE6B6>`__.
+-  Submit bugs or requests in the
+   `Issues <https://github.com/SanPen/GridCal/issues>`__ section.
+-  Simply email `santiago@gridcal.org <santiago@gridcal.org>`__
+
+Contents
+=========
+
+.. toctree::
+    :maxdepth: 1
+
+    rst_source/license
+    rst_source/getting_started
+    rst_source/tutorials/tutorials_module
+    rst_source/data_sheets
+    rst_source/theory_index
+    rst_source/gui
+    rst_source/development
+    rst_source/benchmarks
+    rst_source/troubleshooting
+    rst_source/other_data_models
+    rst_source/change_log
+    rst_source/api/auto/modules
+
+Example Files
+----------------
+
+Below you can find the link to some files that you can download of finished systems:
+
+ - **Distribution Grid** can be found `here <https://github.com/SanPen/GridCal/blob/master/Grids_and_profiles/grids/Some%20distribution%20grid.gridcal>`__.
+ - **Line HVDC Grid** can be found `here <https://github.com/SanPen/GridCal/blob/master/Grids_and_profiles/grids/LineHVDCGrid.gridcal>`__.
+ - **Iwamoto's 11 Bus** can be found `here <https://github.com/SanPen/GridCal/blob/master/Grids_and_profiles/grids/Iwamoto's%2011%20Bus.gridcal>`__.
+ - **Illinois 200 Bus** can be found `here <https://github.com/SanPen/GridCal/blob/master/Grids_and_profiles/grids/Illinois%20200%20Bus.gridcal>`__.
+ - **IEEE 57 & IEEE &14 DC Grid** can be found `here <https://github.com/SanPen/GridCal/blob/master/Grids_and_profiles/grids/IEEE57%2BIEEE14%20DC%20grid.gridcal>`__.
+ - **IEEE 39 Grid** can be found `here <https://github.com/SanPen/GridCal/blob/master/Grids_and_profiles/grids/IEEE39.gridcal>`__.
+ - **IEEE 9 Grid** can be found `here <https://github.com/SanPen/GridCal/blob/master/Grids_and_profiles/grids/IEEE%209%20Bus.gridcal>`__.
+ - **IEEE 30 Grid** can be found `here <https://github.com/SanPen/GridCal/blob/master/Grids_and_profiles/grids/IEEE%2030%20Bus.gridcal>`__.
+
+More resources can be found `here <https://github.com/SanPen/GridCal/blob/master/Grids_and_profiles>`__.
+
+Disclaimer
+----------
+
+All trademarks mentioned in the documentation or the source code belong
+to their respective owners.
+
+.. |image0| image:: pics/GridCal.png
+.. |Codacy Badge| image:: https://api.codacy.com/project/badge/Grade/75e794c9bcfd49bda1721b9ba8f6c790
+   :target: https://app.codacy.com/app/SanPen/GridCal?utm_source=github.com&utm_medium=referral&utm_content=SanPen/GridCal&utm_campaign=Badge_Grade_Dashboard
+.. |Documentation Status| image:: https://readthedocs.org/projects/gridcal/badge/?version=latest
+   :target: https://gridcal.readthedocs.io/en/latest/?badge=latest
+.. |Build Status| image:: https://travis-ci.org/SanPen/GridCal.svg?branch=master
+   :target: https://travis-ci.org/SanPen/GridCal
+.. |DOI| image:: https://www.zenodo.org/badge/49583206.svg
+   :target: https://www.zenodo.org/badge/latestdoi/49583206
+.. |Downloads| image:: https://static.pepy.tech/personalized-badge/gridcal?period=total&units=abbreviation&left_color=grey&right_color=green&left_text=Downloads
+   :target: https://pepy.tech/project/gridcal
